2f3d00702fb1d9cc3b3f7ece7a2396b3045bf08c
asrori hariri <asrori.hariri@gmail.com>
2023-12-26 08:39:08 +0700

    add comment 2 [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // comment
+// comment 2
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
